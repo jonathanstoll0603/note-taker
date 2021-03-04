24e24ed6248ac7f6870f878d357b0678e25812d2
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-03-04 00:46:08 -0500

    added html route for json file, added get route in apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,28 @@
-const notesData = require('../db/db');
-
+const fs = require('fs');
+const uniqid = require('uniqid');
 // Exports all of our app.get methods for our API database calls as a function to be used in server.js
 module.exports = (app) => {
   
+  const notesData = [];
+
   // JSON data page of the notes that have been stored in the db.json file. 
   app.get('/api/notes', (req, res) => {
-    // res.sendFile(path.join(__dirname, '../db/db.json'));
+
+    // reads from the db.json file and stores it in a variable
+    notesData = fs.readFileSync('../db/db.json');
+    // parses the db.json file and places it into an the empty notesdata array
+    notesData = JSON.parse(notesData)
+
+    console.log('Request for API data successful.')
+
     res.json(notesData);
-  });
+
+  })
+  // .catch((err) => console.error(err));
 
   app.post('/api/notes', (req, res) => {
-    notesData.push(req.body);
-    res.json(true);
+    // notesData.push(req.body);
+    // res.json(true);
   });
 
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,16 +3,14 @@ const path = require('path');
 // Exports all of our app.get methods for our html pages as a function to be used in server.js
 module.exports = (app) => {
 
-  // for the home page
-  app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
-  })
-
   // for the notes page
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
   });
 
+  app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../db/db.json'))
+  })
   // If no matching route is found default to home
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
