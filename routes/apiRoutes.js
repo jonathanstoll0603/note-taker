c79ff5b0ae286a0273b33c60999a63fd18031ebd
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-03-05 00:19:42 -0500

    added post route that appends user notes to side bar. Note appeneded with unique id from iniqid npm found in package.json file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,26 +3,51 @@ const uniqid = require('uniqid');
 // Exports all of our app.get methods for our API database calls as a function to be used in server.js
 module.exports = (app) => {
   
-  const notesData = [];
+  let notesData = [];
 
   // JSON data page of the notes that have been stored in the db.json file. 
   app.get('/api/notes', (req, res) => {
 
-    // reads from the db.json file and stores it in a variable
-    notesData = fs.readFileSync('../db/db.json');
-    // parses the db.json file and places it into an the empty notesdata array
-    notesData = JSON.parse(notesData)
-
-    console.log('Request for API data successful.')
-
-    res.json(notesData);
-
-  })
-  // .catch((err) => console.error(err));
-
-  app.post('/api/notes', (req, res) => {
-    // notesData.push(req.body);
-    // res.json(true);
+    notesData = fs.readFileSync('./db/db.json');
+    notesData = JSON.parse(notesData);
+    res.json(notesData)
   });
 
+  app.get('/api/notes/:id', (req, res) => {
+    const noteID = req.params.id;
+
+    notesData.forEach(note => {
+      if (note === noteID) {
+        res.json(notesData)
+      } else {
+        return console.log('err')
+      }
+    })
+
+    console.log(noteID);
+  });
+
+  app.post('/api/notes', (req, res) => {
+    // Reads the json database and then parses it
+    notesData = fs.readFileSync('./db/db.json');
+    notesData = JSON.parse(notesData);
+
+    // setting a new variable equal to the json body in order to automatically add a inique ID key to each note that is posted by the user
+    const addNoteID = req.body;
+    // adds a key of id to the json post that is equal to a unique id set by the uniqid npm package
+    addNoteID.id = uniqid();
+
+    // pushes the raw request body into the database and then stringifies it
+    notesData.push(req.body);
+    notesData = JSON.stringify(notesData);
+
+    // writes the new information into the json database which will be displayed to the user
+    fs.writeFile('./db/db.json', notesData, (err) => {
+      if (err) throw err;
+      console.log('Data pushed to db.json file.');
+
+      // send json object back to client (through api http)
+      res.json(notesData);
+    });
+  });
 };
