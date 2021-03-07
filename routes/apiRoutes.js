5c7fe8c9660d6883e1a1788cfb562beab6e65e2a
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-03-06 19:24:36 -0500

    fixed bug that would return the selected note to the main noteTitle/noteBody section

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,25 +6,21 @@ module.exports = (app) => {
   let notesData = [];
 
   // JSON data page of the notes that have been stored in the db.json file. 
-  app.get('/api/notes', (req, res) => {
+  app.get("/api/notes", function(err, res) {
+    try {
+      // reads the notes from json file
+      notesData = fs.readFileSync("./db/db.json", "utf8");
+      // parse it so notesData is an array of objects
+      notesData = JSON.parse(notesData);
+  
+      // error handling
+    } catch (err) {
+      console.log(err);
+    }
+    console.log("GET request for /api/notes was successful.");
 
-    notesData = fs.readFileSync('./db/db.json');
-    notesData = JSON.parse(notesData);
-    res.json(notesData)
-  });
-
-  app.get('/api/notes/:id', (req, res) => {
-    const noteID = req.params.id;
-
-    notesData.forEach(note => {
-      if (note === noteID) {
-        res.json(notesData)
-      } else {
-        return console.log('err')
-      }
-    })
-
-    console.log(noteID);
+    // send object to the browser
+    res.json(notesData);
   });
 
   app.post('/api/notes', (req, res) => {
@@ -47,7 +43,38 @@ module.exports = (app) => {
       console.log('Data pushed to db.json file.');
 
       // send json object back to client (through api http)
-      res.json(notesData);
+      res.json(JSON.parse(notesData));
     });
+  });
+
+  app.delete("/api/notes/:id", function(req, res) {
+    try {
+      //  reads the json file
+      notesData = fs.readFileSync("db/db.json", "utf8");
+
+      // parse the data to get an array of the objects
+      notesData = JSON.parse(notesData);
+
+      // delete the old note from the array object
+      notesData = notesData.filter((note) => {
+        return note.id != req.params.id;
+      });
+      // make it string(stringify)so you can write it to the file
+      notesData = JSON.stringify(notesData);
+
+      // write the new notes to the file
+      fs.writeFile("db/db.json", notesData, "utf8", function(err) {
+        // error handling
+        if (err) throw err;
+      });
+  
+      console.log("DELETE request was successful.");
+      // turn it back into an array object and send to the client
+      res.send(JSON.parse(notesData));
+  
+      // handles errors
+    } catch (err) {
+      throw err;
+    }
   });
 };
